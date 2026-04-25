7476e747bf68536afbb2ca5768be71c2ef6a3621
AugustHuangMX <augusthuangmx@outlook.com>
2026-04-25 17:33:21 +0100

    Quartz sync: Apr 25, 2026, 5:33 PM

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -48,6 +48,10 @@ export default (() => {
             {cfg.theme.typography.title && (
               <link rel="stylesheet" href={googleFontSubsetHref(cfg.theme, cfg.pageTitle)} />
             )}
+            <link
+              href="https://fonts.googleapis.com/css2?family=LXGW+WenKai+TC:wght@300;400;700&display=swap"
+              rel="stylesheet"
+            />
           </>
         )}
         <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin="anonymous" />

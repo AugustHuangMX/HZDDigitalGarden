d8149329bf053f18ad8a4c2c467c07afc54b4919
AugustHuangMX <augusthuangmx@outlook.com>
2026-04-25 16:40:19 +0100

    Quartz sync: Apr 25, 2026, 4:40 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "HZD's Digital Garden",
+    pageTitle: "August's Digital Garden",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "hzd-digital-garden.vercel.app",
+    baseUrl: "august-digital-garden.vercel.app",
     ignorePatterns: ["private", "templates", ".obsidian","copilot"],
     defaultDateType: "modified",
     theme: {
@@ -29,7 +29,7 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
+          light: "#f5f5dc",
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
@@ -40,7 +40,7 @@ const config: QuartzConfig = {
           textHighlight: "#fff23688",
         },
         darkMode: {
-          light: "#161618",
+          light: "#2a2a25",
           lightgray: "#393639",
           gray: "#646464",
           darkgray: "#d4d4d4",
